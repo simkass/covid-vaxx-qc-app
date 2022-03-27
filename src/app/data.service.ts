aa548b78fdd3131e93f05fb87bbbff0e7ec187f8
simkass <simkassab@gmail.com>
2022-03-27 19:17:31 -0400

    Added offline component

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,8 +8,8 @@ import { User } from './user.model'
 })
 export class DataService {
 
-  private API = "https://covid-vaxx-qc-api.herokuapp.com/";
-  //private API = "http://127.0.0.1:5000/";
+  //private API = "https://covid-vaxx-qc-api.herokuapp.com/";
+  private API = "http://127.0.0.1:5000/";
   private ESTABLISHMENTS_API = this.API + "establishments";
   private USER_API = this.API + "user";
   private UNSUB_REQUEST_API = this.API + "unsubscribe-request";
